8fd3363decf69e334d2cfaf60d5c6f17ad8b352c
MohitJiDeshmukh <mohitdeshmukh203@gmail.com>
2024-07-03 01:31:29 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './App.css';
+import HomePage from './pages/HomePage';
+import { BrowserRouter, Route , Routes} from 'react-router-dom';
+
+import AboutUs from './pages/AboutUs';
+import Services from './pages/Services';
+import ContactUs from './pages/ContactUs';
+import Gallery from './pages/Gallery';
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter  >
+    
+    <Routes>
+      <Route exact path="/" element={<HomePage />} />
+      <Route exact path="/about" element={<AboutUs />} />
+      <Route exact path="/services" element={<Services />} />
+      <Route exact path="/gallery" element={<Gallery/>} />
+      <Route exact path="/contact" element={<ContactUs/>} />
+     
+      
+      
+     
+    </Routes>
+  </BrowserRouter>
+    </div>
+  );
+}
+
+
+export default App;
